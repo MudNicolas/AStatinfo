e189cd41de16b1eff9fa155be1ff804a16c1932d
Nicolas <1075039737@qq.com>
2021-09-13 21:37:15 +0800

    完成预测模块

--- server/router/api.js
+++ server/router/api.js
@@ -3,7 +3,20 @@ let Router = require("express")
 let remote = require("./remote/index.js")
 let local = require("./local/index.js")
 
+let api_config = require("#root/config/api_config.json")
+
 let router = Router()
+
+router.use((req, res, next) => {
+    let { name } = req.query
+    let { query } = api_config
+    let e = query.find(i => i.name === name)
+    if (!e) {
+        return res.status(404).end()
+    }
+    req.tuid = e.uid
+    next()
+})
 
 router.use("/remote", remote)
 router.use("/local", local)
--- webpage/src/utils/request.js
+++ webpage/src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const service = axios.create({
+    baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
     withCredentials: false,
     timeout: 5000,
 })
@@ -17,7 +18,7 @@ service.interceptors.request.use(
 
 service.interceptors.response.use(
     response => {
-        return response
+        return response.data
     },
     error => {
         console.log(error)
